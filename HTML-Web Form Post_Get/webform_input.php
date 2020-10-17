04cbaa9dfdbad621d4ad1bd930206311caf70367
Albert Chandra <35249843+albertchandra18@users.noreply.github.com>
2020-10-17 08:42:05 +0700

    Add files via upload

---
+++ HTML-Web Form Post_Get/webform_input.php
@@ -0,0 +1,23 @@
+<?php
+
+//$namadepan = $_POST['namadepan'];
+//$namabelakang = $_POST['namabelakang'];
+
+//$gender = $_POST['gender'];
+
+$namadepan = $_GET['namadepan'];
+$namabelakang = $_GET['namabelakang'];
+
+$gender = $_GET['gender'];
+
+$kabupaten = $_GET['kabupaten'];
+
+//print_r($namadepan,$namabelakang,$gender);
+
+echo $namadepan;
+echo $namabelakang;
+echo $gender;
+echo $kabupaten;
+
+
+?>
